4b8d618c6c34d30bb7b1e06ff6a1b46d6fcb63f6
Joe Dai <dm.joedai@gmail.com>
2023-01-05 22:54:21 -0500

    Experimental voltage measurement code

--- Code/platformio/PicoPowerMeter/src/main.cpp
+++ Code/platformio/PicoPowerMeter/src/main.cpp
@@ -2,8 +2,6 @@
 #include "SPI.h"
 #include <TFT_eSPI.h>
 
-#define CGRAM_OFFSET
-
 // Default color definitions
 #define TFT_BLACK       0x0000      /*   0,   0,   0 */
 #define TFT_NAVY        0x000F      /*   0,   0, 128 */
@@ -30,33 +28,63 @@
 #define TFT_SKYBLUE     0x867D      /* 135, 206, 235 */
 #define TFT_VIOLET      0x915C      /* 180,  46, 226 */
 
+#define PS_PIN      23
+#define PIN_VOUTDIV 26
+#define PIN_GNDREF  28
+
 TFT_eSPI tft = TFT_eSPI();
 
 double voltage = 0.0;
 double current = 0.0;
-double power = 0.0;
+// double power = 0.0;
 double energy = 0.0;
+double* power = NULL;
+// PicoAnalogCorrection pico(12, 3.3);
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_VOUTDIV, INPUT);
+  pinMode(PIN_GNDREF, INPUT);
+  // pico.calibrateAdc(PIN_GNDREF, PIN_VOUTDIV, 5000);
   tft.init();
   tft.setRotation(1);
-  tft.setTextSize(2);
+  tft.setTextSize(1);
   tft.fillScreen(TFT_BLACK);
+  analogReadResolution(12);
+  digitalWrite(PS_PIN, HIGH);
 }
 
 void loop() {
+  double sum=0.0;
+  for(int i = 0; i< 1000; i++){
+    sum += (analogRead(PIN_VOUTDIV) - analogRead(PIN_GNDREF));
+    delay(1);
+  }
+  sum /= 1000.0;
+
+  voltage = (((sum) * (3.295 / 4096.0) / 0.130383) - 0.22) * 1.048365;
+  // voltage *= 1.0577;
+  // voltage = analogRead(PIN_ADC0);
+
   tft.setCursor(3, 3);
-  tft.print("V: 0.000 V");
+  char buf[16];
+  sprintf(buf, "V: %.4f V", voltage);
+  tft.print("                ");
+  tft.setCursor(3, 3);
+  tft.print(buf);
+  // strcpy(buf, "             \0");
+  // tft.print("V: 0.000 V");
 
   tft.setCursor(3, 23);
-  tft.print("I: 0.000 A");
+  // tft.print(pico.analogRead(PIN_GNDREF));
+  // tft.print("I: 0.000 A");
 
   tft.setCursor(3, 43);
-  tft.print("P: 0.000 W");
+  // tft.print(pico.analogCRead(PIN_GNDREF, 10));
+  // tft.print("P: 0.000 W");
 
   tft.setCursor(3, 63);
-  tft.print("E: 0.000 Wh");
+  // tft.print("E: 0.000 Wh");
 
-  delay(100);
+  delay(10);
 }
